a1c29192beee7168b0a2a9321dce24509a3610d7
ilian.twister <ghrom07@yandex.ru>
2019-09-20 01:19:25 +0300

    - new task bit_differences.py

--- bit_differences.py
+++ bit_differences.py
@@ -1,2 +1,16 @@
-def something():
-    pass
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    numbers = [int(x) for x in input().split()]
+
+    all = 0
+    count = 0
+    for degree in range(32):
+        for num in numbers:
+            if num & (1 << degree) == 0:
+                count += 1
+        all += (len(numbers) - count) * count * 2
+        count = 0
+
+    print(int(all % (1e9 + 7)))
